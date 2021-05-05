1fa7068f359979acb1e025f5aba50a3fc8830ae1
carmo-ga <ga.karmo@gmail.com>
2021-05-05 01:23:07 +0000

    (04/05/21) Ícones, estilos e rotas

--- karangos/src/App.js
+++ karangos/src/App.js
@@ -2,48 +2,86 @@
 import './App.css';
 import Button from '@material-ui/core/Button'
 
-  <div className="App">
-      <header className="App-header">
+<div className="App">
+    <header className="App-header">
         <h1>Projeto Karangos</h1>
         <Button variant="contained" color="primary">Clique aqui</Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer">
+            Learn React
         </a>
-      </header>
-  </div> */
+    </header>
+</div> */
 
 import TopBar from './ui/TopBar'
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import FooterBar from './ui/FooterBar'
+import { createMuiTheme, ThemeProvider, makeStyles } from '@material-ui/core/styles';
 import yellow from '@material-ui/core/colors/yellow';
 import pink from '@material-ui/core/colors/pink';
+import { Box } from '@material-ui/core';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import KarangosList from './routed/KarangosList';
+import KarangosForm from './routed/KarangosForm'
 
 const theme = createMuiTheme({
-  palette: {
-    type: 'dark',
-    primary: {
-      main: yellow[500],
+    palette: {
+        type: 'dark',
+        primary: {
+            main: yellow[500],
+        },
+        secondary: {
+            main: pink[500],
+        },
     },
-    secondary: {
-      main: pink[500],
-    },
-  },
 });
 
+const useStyles = makeStyles((theme) => ({
+    box: {
+        backgroundColor: theme.palette.background.default,
+        minHeight: '100vh'  // 100% da altura da área visível
+    },
+    routed: {
+        padding: '25px',
+        color: theme.palette.text.primary,
+        fontFamily: theme.typography.fontFamily
+    }
+}));
+
+function Main() {
+    const classes = useStyles();
+    return (
+        <Box className={classes.box}>
+            <BrowserRouter>
+                <TopBar />
+                <Box id="routed" className={classes.routed}>
+                    <Switch>
+                        <Route path="/list">
+                            <KarangosList />
+                        </Route>
+                        <Route path="/new">
+                            <KarangosForm />
+                        </Route>
+                    </Switch>
+                </Box>
+                <FooterBar />
+            </BrowserRouter>
+        </Box>
+    )
+}
+
 function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <TopBar />
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <Main />
+        </ThemeProvider>
+    );
 }
 
 export default App;
